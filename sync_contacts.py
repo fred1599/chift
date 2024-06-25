facf07a2752155f94b1ddd1540481e312067bf58
Fred <frederic.juppont@skillcorner.com>
2024-06-25 15:19:42 +0200

    Fix database connection issue

--- sync_contacts.py
+++ sync_contacts.py
@@ -1,10 +1,13 @@
 import os
 import xmlrpc.client
 import psycopg2
-import time
 import dj_database_url
-
 from dotenv import load_dotenv
+import logging
+
+# Configurer la journalisation
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Chargement des variables d'environnement
 load_dotenv()
@@ -17,41 +20,68 @@ api_key = os.environ["API_KEY_ODOO"]
 
 # Configuration PostgreSQL
 database_url = os.environ["DATABASE_URL"]
-db_config = dj_database_url.config(default=database_url)
-dsn = f"dbname={db_config['NAME']} user={db_config['USER']} password={db_config['PASSWORD']} host={db_config['HOST']} port={db_config['PORT']} sslmode=require"
+logger.info("DATABASE_URL: %s", database_url)
 
-conn = psycopg2.connect(dsn)
-
-cursor = conn.cursor()
+try:
+    db_config = dj_database_url.parse(database_url)
+    conn = psycopg2.connect(
+        dbname=db_config['dbname'],
+        user=db_config['user'],
+        password=db_config['password'],
+        host=db_config['host'],
+        port=db_config['port'],
+        sslmode='require'
+    )
+    cursor = conn.cursor()
+    logger.info("Successfully connected to the database")
+except Exception as e:
+    logger.error("Failed to connect to the database: %s", e)
+    raise
 
 # Connexion à Odoo
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, api_key, {})
-models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+try:
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    uid = common.authenticate(db, username, api_key, {})
+    models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    logger.info("Successfully connected to Odoo")
+except Exception as e:
+    logger.error("Failed to connect to Odoo: %s", e)
+    raise
 
 def sync_contacts():
-    # Récupérer les contacts depuis Odoo
-    contact_ids = models.execute_kw(db, uid, api_key, 'res.partner', 'search', [[['is_company', '=', True]]], {'offset': 0, 'limit': 10})
-    contacts = models.execute_kw(db, uid, api_key, 'res.partner', 'read', [contact_ids])
+    try:
+        # Récupérer les contacts depuis Odoo
+        contact_ids = models.execute_kw(db, uid, api_key, 'res.partner', 'search', [[['is_company', '=', True]]], {'offset': 0, 'limit': 10})
+        contacts = models.execute_kw(db, uid, api_key, 'res.partner', 'read', [contact_ids])
+        logger.info("Retrieved contacts from Odoo")
 
-    for contact in contacts:
-        contact_id = contact['id']
-        name = contact['name']
-        email = contact.get('email', '')
-        phone = contact.get('phone', '')
+        for contact in contacts:
+            contact_id = contact['id']
+            name = contact['name']
+            email = contact.get('email', '')
+            phone = contact.get('phone', '')
 
-        # Vérifier si le contact existe déjà
-        cursor.execute('SELECT id FROM contacts_contact WHERE odoo_id = %s', (contact_id,))
-        result = cursor.fetchone()
+            # Vérifier si le contact existe déjà
+            cursor.execute('SELECT id FROM contacts_contact WHERE odoo_id = %s', (contact_id,))
+            result = cursor.fetchone()
 
-        if result:
-            # Mettre à jour le contact
-            cursor.execute('''
-                UPDATE contacts_contact SET name = %s, email = %s, phone = %s WHERE odoo_id = %s
-            ''', (name, email, phone, contact_id))
-        else:
-            # Insérer le contact
-            cursor.execute('''
-                INSERT INTO contacts_contact (odoo_id, name, email, phone) VALUES (%s, %s, %s, %s)
-            ''', (contact_id, name, email, phone))
+            if result:
+                # Mettre à jour le contact
+                cursor.execute('''
+                    UPDATE contacts_contact SET name = %s, email = %s, phone = %s WHERE odoo_id = %s
+                ''', (name, email, phone, contact_id))
+                logger.info("Updated contact with odoo_id %s", contact_id)
+            else:
+                # Insérer le contact
+                cursor.execute('''
+                    INSERT INTO contacts_contact (odoo_id, name, email, phone) VALUES (%s, %s, %s, %s)
+                ''', (contact_id, name, email, phone))
+                logger.info("Inserted contact with odoo_id %s", contact_id)
 
+        conn.commit()
+    except Exception as e:
+        logger.error("Failed to sync contacts: %s", e)
+        conn.rollback()
+
+# Appeler la fonction de synchronisation
+sync_contacts()
